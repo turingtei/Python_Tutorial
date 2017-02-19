2e67308f105c085c96566601b96d43e31b2f1fe9
turingtei <turingtei@gmail.com>
2017-02-19 12:10:17 -0500

    Add new file and complete assignment

---
+++ lesson_4a_ex1.py
@@ -0,0 +1,7 @@
+'''
+Create a list that contains first 6 prime numbers in ascending order.
+(This list should be created manually.) Print out the 2nd, 4th, and 6th
+numbers in this list. 
+'''
+prime = [2,3,5,7,11,13]
+print (prime[1]),(prime[3]),(prime[5])
