896bbd415d96ad3d687b7f613a2691093ccab9b0
turingtei <turingtei@gmail.com>
2017-02-18 20:57:55 -0500

    Add 'start','stop','reset' buttons

--- lesson_3b_ex2.py
+++ lesson_3b_ex2.py
@@ -18,16 +18,33 @@ counter = 0
 # Timer handler
 def tick():
     global counter
-    print counter
+    print (counter)
     counter += 1
     
 # Event handlers for buttons    
+def start_handler():
+	timer.start()
+
+def stop_handler():
+	timer.stop()
+
+def reset_handler():
+	global counter
+	timer.stop()
+	counter = 0
+
+
 
         
 # Create frame and timer
 frame = simplegui.create_frame("Counter with buttons", 200, 200)
 timer = simplegui.create_timer(1000, tick)
+start_button = frame.add_button('Start', start_handler)
+stop_button = frame.add_button('Stop', stop_handler)
+reset_button = frame.add_button('Reset',reset_handler)
+
+frame.start()
 
 # Start timer
-timer.start()
+
 
