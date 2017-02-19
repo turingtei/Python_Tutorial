7f33bdf74d25bb6f4bcab8436e1da84c1e270e30
turingtei <turingtei@gmail.com>
2017-02-18 20:45:40 -0500

    Add new file

---
+++ lesson_3b_ex1.py
@@ -0,0 +1,27 @@
+'''The following program should count upward from zero.
+Print the counter values 0, 1, 2, … to the console.
+Add two lines of Python to make this program work.
+Hint: Add a global variable to a timer callback, and start a timer. '''
+
+# Counter ticks
+
+###################################################
+# Student should add code where relevant to the following.
+
+try:
+    import simplegui
+except:
+    import simpleguitk as simplegui
+
+counter = 0
+
+# Timer handler
+def tick():
+    print counter
+    counter += 1
+
+# create timer
+timer = simplegui.create_timer(1000, tick)
+
+
+
