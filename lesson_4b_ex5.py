35fe450f3ad053bc26abccfac7c36a3d6232d233
turingtei <turingtei@gmail.com>
2017-02-20 22:02:19 -0600

    Make readibility adjustments to code

--- lesson_4b_ex5.py
+++ lesson_4b_ex5.py
@@ -17,7 +17,7 @@ HALF_PAD_WIDTH = PAD_WIDTH / 2
 HALF_PAD_HEIGHT = PAD_HEIGHT / 2
 LEFT = False
 RIGHT = True
-ball_pos = [WIDTH/2,HEIGHT/2]
+ball_pos = [WIDTH / 2,HEIGHT / 2]
 ball_vel = [0,0]
 
 
@@ -36,15 +36,18 @@ def new_game():
     global paddle1_pos, paddle2_pos, paddle1_vel, paddle2_vel  # these are numbers
     global score1, score2
 
-    paddle1_pos = HEIGHT/2
-    paddle2_pos = HEIGHT/2
+    paddle1_pos = HEIGHT / 2
+    paddle2_pos = HEIGHT / 2
     paddle1_vel = 0
     paddle2_vel = 0
+    score1 = 0
+    score2 = 0
 
+    #randomize starting vector
     starting = random.randrange(1,3)
     if starting == 1:
         spawn_ball(RIGHT)
-    elif starting ==2:
+    elif starting == 2:
         spawn_ball(LEFT)
 
 def draw(canvas):
@@ -61,7 +64,7 @@ def draw(canvas):
     ball_pos[0] += ball_vel[0]
     ball_pos[1] += ball_vel[1]
     if ball_pos[0] < BALL_RADIUS+PAD_WIDTH:
-        if (paddle1_pos+HALF_PAD_HEIGHT)>ball_pos[1] and (paddle1_pos-HALF_PAD_HEIGHT)<ball_pos[1]:
+        if (paddle1_pos + HALF_PAD_HEIGHT) > ball_pos[1] and (paddle1_pos - HALF_PAD_HEIGHT) < ball_pos[1]:
             ball_vel[0] = -ball_vel[0]
             ball_vel[1] = -ball_vel[1]
 
@@ -69,34 +72,34 @@ def draw(canvas):
             ball_pos = [WIDTH/2,HEIGHT/2]
             ball_vel = [random.randrange(1,4),random.randrange(-3,-1)]
         
-    elif ball_pos[0] > (WIDTH-BALL_RADIUS-PAD_WIDTH):
-        if (paddle2_pos+HALF_PAD_HEIGHT)>ball_pos[1] and (paddle2_pos-HALF_PAD_HEIGHT)<ball_pos[1]:
+    elif ball_pos[0] > (WIDTH - BALL_RADIUS - PAD_WIDTH):
+        if (paddle2_pos + HALF_PAD_HEIGHT) > ball_pos[1] and (paddle2_pos - HALF_PAD_HEIGHT) < ball_pos[1]:
             ball_vel[0] = -ball_vel[0]
             ball_vel[1] = -ball_vel[1]
 
         else:
-            ball_pos = [WIDTH/2,HEIGHT/2]
+            ball_pos = [WIDTH / 2,HEIGHT / 2]
             ball_vel = [random.randrange(1,4),random.randrange(-3,-1)]
         
     if ball_pos[1] < BALL_RADIUS:
         ball_vel[1] = -ball_vel[1]
-    elif ball_pos[1] > (HEIGHT -BALL_RADIUS):
+    elif ball_pos[1] > (HEIGHT - BALL_RADIUS):
         ball_vel[1] = -ball_vel[1]
 
     # draw ball
     canvas.draw_circle(ball_pos,BALL_RADIUS,1,'Red','Red')
     
     # update paddle's vertical position, keep paddle on the screen
-    if ((paddle1_pos + paddle1_vel) >= (HALF_PAD_HEIGHT)) and ((paddle1_pos + paddle1_vel) <= (HEIGHT- HALF_PAD_HEIGHT)):
-        paddle1_pos+=paddle1_vel
+    if ((paddle1_pos + paddle1_vel) >= (HALF_PAD_HEIGHT)) and ((paddle1_pos + paddle1_vel) <= (HEIGHT - HALF_PAD_HEIGHT)):
+        paddle1_pos += paddle1_vel
 
-    if ((paddle2_pos + paddle2_vel) >= (HALF_PAD_HEIGHT)) and ((paddle2_pos + paddle2_vel) <= (HEIGHT- HALF_PAD_HEIGHT)):
-        paddle2_pos+=paddle2_vel
+    if ((paddle2_pos + paddle2_vel) >= (HALF_PAD_HEIGHT)) and ((paddle2_pos + paddle2_vel) <= (HEIGHT - HALF_PAD_HEIGHT)):
+        paddle2_pos += paddle2_vel
     
 
     # draw paddles
-    canvas.draw_polygon([[0, paddle1_pos+HALF_PAD_HEIGHT], [PAD_WIDTH,paddle1_pos+HALF_PAD_HEIGHT], [PAD_WIDTH, paddle1_pos-HALF_PAD_HEIGHT], [0, paddle1_pos-HALF_PAD_HEIGHT]], 1, 'Yellow', 'Yellow')
-    canvas.draw_polygon([[WIDTH, paddle2_pos+HALF_PAD_HEIGHT], [WIDTH-PAD_WIDTH,paddle2_pos+HALF_PAD_HEIGHT], [WIDTH-PAD_WIDTH, paddle2_pos-HALF_PAD_HEIGHT], [WIDTH, paddle2_pos-HALF_PAD_HEIGHT]], 1, 'Blue', 'Blue')
+    canvas.draw_polygon([[0, paddle1_pos + HALF_PAD_HEIGHT], [PAD_WIDTH,paddle1_pos + HALF_PAD_HEIGHT], [PAD_WIDTH, paddle1_pos - HALF_PAD_HEIGHT], [0, paddle1_pos - HALF_PAD_HEIGHT]], 1, 'Yellow', 'Yellow')
+    canvas.draw_polygon([[WIDTH, paddle2_pos + HALF_PAD_HEIGHT], [WIDTH - PAD_WIDTH,paddle2_pos + HALF_PAD_HEIGHT], [WIDTH - PAD_WIDTH, paddle2_pos - HALF_PAD_HEIGHT], [WIDTH, paddle2_pos - HALF_PAD_HEIGHT]], 1, 'Blue', 'Blue')
     
     # determine whether paddle and ball collide
     #if (paddle2_pos+HALF_PAD_HEIGHT)>ball_pos[1] and (paddle2_pos-HALF_PAD_HEIGHT)<ball_pos[1]:
