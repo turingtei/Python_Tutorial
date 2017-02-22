f4e8d97d3cb5c4043da1c9af3b9b21fd5e8b4a35
turingtei <turingtei@gmail.com>
2017-02-21 21:35:57 -0600

    Make cipher

--- lesson_5b_01.py
+++ lesson_5b_01.py
@@ -16,34 +16,43 @@ except:
 
 CIPHER = {'a': 'x', 'b': 'c', 'c': 'r', 'd': 'm', 'e': 'l'}
 
-message = ""
+message = " "
 
 # Encode button
 def encode():
     emsg = ""
     for ch in message:
         emsg += CIPHER[ch]
-    print message, "encodes to", emsg
+    print (message, "encodes to", emsg)
 
 # Decode button
 def decode():
-    dmsg = ""
-    for ch in message:
-        for key, value in CIPHER.items():
-            if ch == value:
-                dmsg += key
-    print message, "decodes to", dmsg
+  dmsg = ""
+  '''
+  for ch in message:
+    for key, value in CIPHER.items():
+      if ch == value:
+        dmsg += key
+'''
+  
+  
+  for ch in message:
+    for key, value in CIPHER.items():
+      if ch == value:
+        dmsg += key
 
+  print (message, "decodes to", dmsg)
 # Update message input
+
 def newmsg(msg):
-    global message
-    message = msg
-    label.set_text(msg)
+  global message
+  message = msg
+  label.set_text(msg)
     
 # Create a frame and assign callbacks to event handlers
 frame = simplegui.create_frame("Cipher", 2, 200, 200)
 frame.add_input("Message:", newmsg, 200)
-label = frame.add_label("", 200)
+label = frame.add_label(" ", 200)
 frame.add_button("Encode", encode)
 frame.add_button("Decode", decode)
 
