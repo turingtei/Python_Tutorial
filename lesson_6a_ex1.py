63ff61adcd836dc81f366b1cad4ec06f253ecb16
turingtei <turingtei@gmail.com>
2017-02-24 22:18:42 -0600

    Exercise 1. Define an empty Tile class

---
+++ lesson_6a_ex1.py
@@ -0,0 +1,42 @@
+'''
+For this set of practice exercises, we will walk you
+through the creation of a Tile class suitable for use
+in your week five mini-project, Memory. This class will
+model the logical behavior of cards/tiles used in Memory.
+Our goal in walking through this design will be to
+understand the syntactic structure of a Python class
+in detail as well as the logic that goes into designing
+a useful class. In Week 6b, we will use this Tile class
+to re-implement Memory in an object-oriented style.
+If, at some point, you feel confused, you may also want
+to watch this video on the Basics of OOP by Julie.
+Many students in previous sessions found it to be helpful.
+
+To begin, your first task is to define a Tile class
+using two lines of Python. For now, the body of this
+class should just be a single pass statement to keep
+Python from throwing an error. As usual in Python,
+the body of the class definition should be indented.
+'''
+
+# Define an empty Tile class 
+
+
+#################################################
+# Student adds code where appropriate
+
+# definition of empty Tile class (use pass in the body)
+
+class Tile():
+  pass    
+    
+###################################################
+# Testing code
+
+print (Tile)
+
+
+####################################################
+# Output of testing code
+
+# <class '__main__.Tile'>
