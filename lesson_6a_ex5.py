12f27e3389b09ef2844f612dcba0e7c76bb252d1
turingtei <turingtei@gmail.com>
2017-02-24 23:13:22 -0600

    Add an exposed field to a Tile object with three methods

---
+++ lesson_6a_ex5.py
@@ -0,0 +1,54 @@
+# Add an exposed field to a Tile object with three methods
+
+#################################################
+# Student adds code where appropriate    
+
+# definition of a Tile class
+class Tile:
+    
+    # definition of intializer
+    def __init__(self, num,exp):
+        self.number = num
+        self.exposed = exp
+        
+    # definition of getter for number
+    def get_number(self):
+        return self.number
+    
+    # check whether tile is exposed
+    def is_exposed(self):
+      return self.exposed
+    
+    # expose the tile
+    def expose_tile(self):
+      self.exposed = True
+      return self.exposed
+    
+    # hide the tile
+    def hide_tile(self):
+      self.exposed = False
+      return self.exposed       
+    
+    
+# create a Tile called my_tile with number 3 that is exposed    
+my_tile = Tile(3, True)
+
+    
+###################################################
+# Testing code
+
+print (my_tile)
+print (my_tile.is_exposed())
+my_tile.hide_tile()
+print (my_tile.is_exposed())
+my_tile.expose_tile()
+print (my_tile.is_exposed())
+
+
+####################################################
+# Output of testing code
+
+#<__main__.Tile object>
+#True
+#False
+#True
