873787193c31e6e5c063bcf03a71e29c8443410b
turingtei <turingtei@gmail.com>
2017-02-24 22:26:33 -0600

    Create class objects

---
+++ lesson_6a_ex2.py
@@ -0,0 +1,41 @@
+'''
+Your next task is to create two instances (versions)
+of a Tile object. Note this is possible even though the
+body of the class definition is currently empty.
+Of course, the objects that you create at this point
+will contain no data. We'll add some data to Tile
+objects in the next problem. Remember that to create
+an instance of a class ClassName , you can use the
+expression ClassName()
+'''
+
+# Create an instance of a Tile object 
+
+# definition of empty Tile class (use pass in the body)
+class Tile:
+    
+    pass
+    
+    
+#################################################
+# Student adds code where appropriate    
+
+my_tile = Tile()
+your_tile = Tile()
+    
+###################################################
+# Testing code 
+
+print (my_tile)
+print (type(my_tile))
+print (your_tile)
+print (type(your_tile))
+
+
+####################################################
+# Output of testing code
+
+#<__main__.Tile object>
+#<class '__main__.Tile'>
+#<__main__.Tile object>
+#<class '__main__.Tile'>
